64b69cdd7fbb9767ca6d50883d852ac8e19623e0
Razfar14 <rajarangkuty14@gmail.com>
2025-07-09 15:26:25 +0700

    Menampilkan Data

--- ArrayStruct_0123/NestedStruct.cpp
+++ ArrayStruct_0123/NestedStruct.cpp
@@ -27,3 +27,15 @@ int main(){
         cout << "Masukkan Provinsi: ";
         cin >> mhs[i].alamat.provinsi;
     }
+
+    cout << endl;
+    cout << "Menampilkan Data " << endl;
+    cout << endl;
+    for (int i = 0; i < 2; i++){
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "Nim: " << mhs[i].nim << endl;
+        cout << "Nama: " << mhs[i].nama << endl;
+        cout << "Kota: " << mhs[i].alamat.kota << endl;
+        cout << "Provinsi: " << mhs[i].alamat.provinsi << endl;
+    }
+}
