04a6abda91cdd151155f0e276003e15e1618679a
Razfar14 <rajarangkuty14@gmail.com>
2025-07-09 15:25:57 +0700

    Membuat objek struct dalam array

--- ArrayStruct_0123/NestedStruct.cpp
+++ ArrayStruct_0123/NestedStruct.cpp
@@ -11,3 +11,19 @@ struct Mahasiswa{
     string nim;
     DetaiAlamat alamat;
 };
+
+int main(){
+
+    // Membuat objek struct dalam array
+    Mahasiswa mhs[2];
+    for (int i = 0; i < 2; i++){
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "Masukkan Nim: ";
+        cin >> mhs[i].nim;
+        cout << "Masukkan Nama: ";
+        cin >> mhs[i].nama;
+        cout << "Masukkan Kota: ";
+        cin >> mhs[i].alamat.kota;
+        cout << "Masukkan Provinsi: ";
+        cin >> mhs[i].alamat.provinsi;
+    }
