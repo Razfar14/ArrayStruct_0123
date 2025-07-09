bc3720832eab09a176289b13da0d6d079e90364f
Razfar14 <rajarangkuty14@gmail.com>
2025-07-09 15:28:09 +0700

    Meminta data

--- ArrayStruct_0123/Struct.cpp
+++ ArrayStruct_0123/Struct.cpp
@@ -6,3 +6,13 @@ struct Mahasiswa{
     string nim;
     string alamat;
 };
+
+int main(){
+    Mahasiswa mhs;
+
+    cout << "Masukkan Nim: ";
+    cin >> mhs.nim;
+    cout << "Masukkan Nama: ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat: ";
+    cin >> mhs.alamat;
