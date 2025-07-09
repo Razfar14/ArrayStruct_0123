2714cce2425f45d3ff10c3204e43bc55099c7644
Razfar14 <rajarangkuty14@gmail.com>
2025-07-09 15:27:16 +0700

    Start

--- ArrayStruct_0123/Struct.cpp
+++ ArrayStruct_0123/Struct.cpp
@@ -1,27 +1,3 @@
 #include <iostream>
 using namespace std;
 
-struct Mahasiswa{
-    string nama;
-    string nim;
-    string alamat;
-};
-
-int main(){
-    Mahasiswa mhs;
-
-    cout << "Masukkan Nim: ";
-    cin >> mhs.nim;
-    cout << "Masukkan Nama: ";
-    cin >> mhs.nama;
-    cout << "Masukkan Alamat: ";
-    cin >> mhs.alamat;
-
-    cout << endl;
-    cout << "Menamplkan Data " << endl;
-    cout << endl;
-
-    cout << "Nim: " << mhs.nim << endl;
-    cout << "Nama: " << mhs.nama << endl;
-    cout << "Alamat: " << mhs.alamat << endl;
-}
