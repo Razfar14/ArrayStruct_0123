bba842f665dae661b8a9cbb184fd7bf0b01919e7
Razfar14 <rajarangkuty14@gmail.com>
2025-07-09 15:23:18 +0700

    //mengisi array dengan menggunakan looping

--- ArrayStruct_0123/Array.cpp
+++ ArrayStruct_0123/Array.cpp
@@ -9,3 +9,14 @@ int nilaiDua[3] = {1,2,3};
 
 //deklarasi array dengan ukuran, tanpa diberi nilai
 string nama[4];
+
+int main(){
+    cout << "Nilai array ke 3 :"<< nilai[2] << endl;
+    nilai[2] = 100;
+    cout << "Nilai array ke 3 :"<< nilai[2] << endl;
+    
+    //mengisi array dengan menggunakan looping
+    for (int i = 0; i < 4; i++){
+        cout << "Masukkan nama ke " << (i + 1) << ": ";
+        cin >> nama[i];
+    }
