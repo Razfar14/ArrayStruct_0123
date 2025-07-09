ba8974a457de06568c82c89037b307fb54c4b8ca
Razfar14 <rajarangkuty14@gmail.com>
2025-07-09 15:25:38 +0700

    Menambahkan Struct

--- ArrayStruct_0123/NestedStruct.cpp
+++ ArrayStruct_0123/NestedStruct.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+struct DetaiAlamat{
+    string kota;
+    string provinsi;
+};
+
+struct Mahasiswa{
+    string nama;
+    string nim;
+    DetaiAlamat alamat;
+};
